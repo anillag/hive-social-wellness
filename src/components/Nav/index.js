b358d637ec699a2d2a8ee2a5b64b9034be4a73c9
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-15 18:53:09 -0400

    added nav component and styled it with reactive hamburger menu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,20 +5,27 @@ const Nav = () => {
   return (
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link to="/TheHive">The Hive</Link>
-          </li>
-          <li>
-            <Link to="/TheBuzz">The Buzz</Link>
-          </li>
-          <li>
-            <Link to="/TheBusyBee">The Busy Bee</Link>
-          </li>
-          <li>
-            <Link to="/TheColony">The Colony</Link>
-          </li>
-        </ul>
+        <section class="top-nav">
+          <div>Logo</div>
+          <input id="menu-toggle" type="checkbox" />
+          <label class="menu-button-container" for="menu-toggle">
+            <div class="menu-button"></div>
+          </label>
+          <ul class="menu">
+            <li>
+              <Link to="/TheHive">The Hive</Link>
+            </li>
+            <li>
+              <Link to="/TheBuzz">The Buzz</Link>
+            </li>
+            <li>
+              <Link to="/TheBusyBee">The Busy Bee</Link>
+            </li>
+            <li>
+              <Link to="/TheColony">The Colony</Link>
+            </li>
+          </ul>
+        </section>
       </nav>
     </div>
   );
