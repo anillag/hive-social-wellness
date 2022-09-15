e6a646b52afadd1b28b39572d7bd57407f3e542b
Charlotte <charlottealiciarichard@gmail.com>
2022-09-14 20:37:02 -0400

    Splash page

--- src/components/SplashPage/index.js
+++ src/components/SplashPage/index.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React from 'react';
 
 const SplashPage = () => {
-  return <div>SplashPage content goes here</div>;
-};
 
-export default SplashPage;
+  return (
+    <div name='splash' className='w-full h-screen bg-[#171718]'>
+        {/* WELCOME splash */}
+      <div className="flex flex-col justify-center items-center w-full h-full"> 
+      <p className='text-3xl  text-[#e1e0d6]' >Honey, this app was created for you.</p>
+      <h1 className='text-5xl  text-[#f0e265]'> HIVE SOCIAL WELLNESS </h1>
+      <p className='text-3xl  text-[#e1e0d6]'> Run and tell your friends </p>
+
+        {/* BUTTON */}
+      <div>
+      <button> 
+         JOIN 
+      </button>
+      </div>
+      <div>
+      
+
+      
+      </div>
+      </div>
+      
+    </div>
+
+
+  )
+}
+
+export default SplashPage
