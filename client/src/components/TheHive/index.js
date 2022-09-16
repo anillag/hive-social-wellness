0895bc46dba2d2b90cb1e531f40da2b3327f05f8
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-16 13:27:57 -0400

    added buttons and style to links in the hive page

--- client/src/components/TheHive/index.js
+++ client/src/components/TheHive/index.js
@@ -2,26 +2,34 @@ import React from "react";
 import { GiTreeBeehive } from "react-icons/gi";
 import { GiHoneycomb } from "react-icons/gi";
 import { CgBee } from "react-icons/cg";
+import { Link } from "react-router-dom";
+
 const TheHive = () => {
   return (
     <div className="hiveFlex">
       <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
-        <button>
-          <GiTreeBeehive />
-          The Buzz!
-        </button>
+        <Link to="/the-buzz">
+          <button>
+            <GiTreeBeehive />
+            The Buzz!
+          </button>
+        </Link>
       </div>
       <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
-        <button>
-          <GiHoneycomb />
-          The Colony!
-        </button>
+        <Link to="/the-colony">
+          <button>
+            <GiHoneycomb />
+            The Colony!
+          </button>
+        </Link>
       </div>
       <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
-        <button>
-          <CgBee />
-          Busy Bee!
-        </button>
+        <Link to="/the-busy-bee">
+          <button>
+            <CgBee />
+            The Busy Bee!
+          </button>
+        </Link>
       </div>
     </div>
   );
