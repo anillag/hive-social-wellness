cb7a0bc2b5912c1a5f24e3f1ab22145a81820464
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-16 17:11:11 -0400

    added text entry and submission button for the buzz page and added styling for both

--- client/src/components/TheBuzz/index.js
+++ client/src/components/TheBuzz/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TheBuzz = () => {
-  return <div>TheBuzz content goes here</div>;
+  return (
+    <div className=" hiveFlex">
+      <form className="text-2xl px-6 py-3 text-[#222]">
+        <textarea
+          placeholder="Tell the Hive what's on your mind..."
+          className="buzz-text text-xs"
+        ></textarea>
+        <button className="rounded-2xl buzz-button flex items-center justify-center ">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default TheBuzz;
