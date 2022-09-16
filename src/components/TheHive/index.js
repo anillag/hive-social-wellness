cbf36106c78e5cf80181a4347987cab9a74117c1
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-15 21:18:45 -0400

    continued setting up divs and styling for hive page

--- src/components/TheHive/index.js
+++ src/components/TheHive/index.js
@@ -1,7 +1,30 @@
 import React from "react";
-
+import { GiTreeBeehive } from "react-icons/gi";
+import { GiHoneycomb } from "react-icons/gi";
+import { CgBee } from "react-icons/cg";
 const TheHive = () => {
-  return <div>TheHive content goes here</div>;
+  return (
+    <div className="hiveFlex">
+      <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
+        <button>
+          <GiTreeBeehive />
+          The Buzz!
+        </button>
+      </div>
+      <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
+        <button>
+          <GiHoneycomb />
+          The Colony!
+        </button>
+      </div>
+      <div className="text-5xl px-6 py-3 text-[#222] hiveDiv">
+        <button>
+          <CgBee />
+          Busy Bee!
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TheHive;
