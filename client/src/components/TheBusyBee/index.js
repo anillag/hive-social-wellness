6eb7a68429e5384c74ea7e9709987cef5fc6b950
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-17 12:09:03 -0400

    added busy bee list base strcuture and some styling /logic

--- client/src/components/TheBusyBee/index.js
+++ client/src/components/TheBusyBee/index.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TheBusyBee = () => {
-  return <div>TheBusyBee content goes here</div>;
+  const [goalField, setGoalField] = useState([
+    {
+      complete: false,
+      goal: "",
+    },
+  ]);
+  // let list = [];
+  const addNewGoal = () => {
+    setGoalField([
+      ...goalField,
+      {
+        complete: false,
+        goal: "",
+      },
+    ]);
+  };
+  const removeGoal = (index) => {
+    const list = [...goalField];
+    list.splice(index, 1);
+    setGoalField(list);
+  };
+  // once backend connects, change logic^ index will be id, set delete goal by id
+  // onBlur save, or save button
+
+  return (
+    <div className="hiveFlex w-full p-5 flex justify-center">
+      <div className="w-1/2 flex-row justify-center">
+        <div className="w-full flex justify-end">
+          <button
+            className="buzz-button rounded-3xl"
+            type="button"
+            onClick={addNewGoal}
+          >
+            Add Goal!
+          </button>
+        </div>
+        <div className="w-full flex-row justify-center">
+          {goalField.map((goal, i) => (
+            <div key={i}>
+              <input type="checkbox" defaultValue={goal.complete} />
+              <input
+                className="buzz-text m-5"
+                type="text"
+                placeholder="Enter A New Goal Here!"
+                defaultValue={goal.goal}
+              />
+              <button type="button" onClick={() => removeGoal(i)} className="">
+                x
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TheBusyBee;
--- client/src/components/TheBuzz/index.js
+++ client/src/components/TheBuzz/index.js
@@ -7,7 +7,6 @@ const TheBuzz = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
   return (
-
     <div>
       <div className=" hiveFlex">
         <form className="text-2xl px-6 py-3 text-[#222]">
